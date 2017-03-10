c8ae4bb596352309ec7ddcbb0ff012f75d9fec0b
unknown <sjain@HP840G3-2165.HAICPM.COM>
2017-03-10 10:48:40 +0530

    added

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganisationsPage } from '../pages/organisations/organisations';
+import { GithubUsers } from '../providers/github-users';
+import { UserDetailsPage } from '../pages/user-details/user-details';
+//import { ChartsModule } from 'ng2-charts';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage,
+	UserDetailsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganisationsPage,
+	UserDetailsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, GithubUsers]
+})
+export class AppModule {}
---
+++ src/providers/github-users.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { User } from '../models/user';
+
+//The @Injectable() decorator is how Angular 2 declares it's services/providers. 
+@Injectable()
+export class GithubUsers {
+  githubApiUrl = 'https://api.github.com';
+
+  constructor(public http: Http) { }
+
+  // Load all github users
+  load(): Observable<User[]> {
+    return this.http.get(`${this.githubApiUrl}/users`)
+      .map(res => <User[]>res.json());
+  }
+  
+  // Get github user by providing login(username)
+  loadDetails(login: string): Observable<User> {
+    return this.http.get(`${this.githubApiUrl}/users/${login}`)
+      .map(res => <User>(res.json()))
+  }
+  
+  // Search for github users  
+  searchUsers(searchParam: string): Observable<User[]> {
+    return this.http.get(`${this.githubApiUrl}/search/users?q=${searchParam}`) 
+      .map(res => <User[]>(res.json().items))
+  }
+}
+
+//We make a request to the github api, and parse the json response with res.json(), 
+//which we then cast as an array of users with <User[]>res.json(). 
+//This is returned as an observable, which we'll subscribe to to see the users.
